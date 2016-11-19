e6d8626f4f1e17764890b53c460990cd009752ad
dx1 <dx1alex@gmail.com>
2016-11-19 15:48:59 +0700

    ...

--- js/test/test.js
+++ js/test/test.js
@@ -1,14 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
 const _1 = require("../");
 const wd = new _1.default({ url: 'http://localhost:9515' });
-wd.getStatus().then(console.log);
-wd.initSession({
-    desiredCapabilities: {
-        browserName: 'chrome'
-    }
-})
-    .then(res => {
-    console.log(res);
-    return wd.openUrl({ url: 'http://google.com' });
-})
-    .catch(console.log);
+main();
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            console.log(yield wd.getStatus());
+            let res = yield wd.initSession({
+                desiredCapabilities: {
+                    browserName: 'chrome'
+                }
+            });
+            console.log(res);
+            yield wd.openUrl({ sessionId: res.sessionId, url: 'http://google.com' });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
--- test/test.ts
+++ test/test.ts
@@ -2,13 +2,18 @@ import Wd from '../'
 
 const wd = new Wd({ url: 'http://localhost:9515' })
 
-wd.getStatus().then(console.log)
-wd.initSession({
-  desiredCapabilities: {
-  browserName: 'chrome'
-}})
-  .then(res => {
+main()
+async function main() {
+  try {
+    console.log(await wd.getStatus())
+    let res = await wd.initSession({
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    })
     console.log(res)
-    return wd.openUrl({url:'http://google.com'})
-  })
-  .catch(console.log)
+    await wd.openUrl({ sessionId: res.sessionId, url: 'http://google.com' })
+  } catch (err) {
+    console.log(err)
+  }
+}
